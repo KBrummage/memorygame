e2eed316fa7d233d639bed372c51b0d27d0bf22a
Muddy Lee <muddy@Muddys-iMac.local>
2018-11-05 18:57:06 -0800

    80% functionality

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ class App extends Component {
   };
 
   randomizeFlowerField = arr =>{
-    var origArr = arr;
+    var origArr = [...arr];
     var tempArr = [];
     while( origArr.length > 0){
-      var i = Math.floor(Math.random()* arr.length)
+      var i = Math.floor(Math.random()* origArr.length)
       tempArr.push(origArr[i])
       origArr.splice(i, 1);
     }
@@ -27,30 +27,38 @@ class App extends Component {
   }
   
   pickFlower = id => {
-    const remainingFlowers = this.state.remaining.filter(flower => flower.id !== id)
+    console.log(id)
+
     
-    let findFlower = remainingFlowers.find(flower => flower.id === id)
+    let findFlower = this.state.remaining.find(flower => flower.id === id)
+    console.log(findFlower)
     if(!findFlower){
       this.setState({
         flowers: this.randomizeFlowerField(flowers),
         headline: "Your bouquet already had that flower",
         currentScore: 0,
-        highScore: ((this.state.currentScore >= this.state.highScore) ? this.state.currentScore : this.state.highScore),
         remaining: flowers
       })      
-      }  else{
-      this.setState({
-      flowers: this.randomizeFlowerField(flowers),
-      currentScore: this.state.currentScore + 1,
-      remaining: remainingFlowers
-      })
-    }
+      } else {
+        const remainingFlowers = this.state.remaining.filter(flower => flower.id !== id)
+        console.log(remainingFlowers);
+        this.setState({
+          flowers: this.randomizeFlowerField(flowers),
+          currentScore: this.state.currentScore + 1,
+          remaining: remainingFlowers,
+          headline: ""
+        })
+      }
   }
 
   render() {
     return (
       <Wrapper>
-        <Title> Flowers </Title>
+        <Title>Flowers 
+          <div>Current Score: {this.state.currentScore}</div>
+          <div>High Score: {this.state.highScore}</div>
+          <div>{this.state.headline}</div>
+        </Title>
         {this.state.flowers.map(flower => (
           <FlowerCard 
             pickFlower= {this.pickFlower}
