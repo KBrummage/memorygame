f481c1f92d0766c92eb56593f99276ecbeb20b82
Muddy Lee <muddy@Muddys-iMac.local>
2018-11-06 23:01:59 -0800

    Updates

---
+++ precache-manifest.8984aa393b5aa181d000a25652b9d4c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "a69eb10483c58ef1088a",
+    "url": "/memorygame/static/js/main.a69eb104.chunk.js"
+  },
+  {
+    "revision": "f843a591dc3acd9073f0",
+    "url": "/memorygame/static/js/1.f843a591.chunk.js"
+  },
+  {
+    "revision": "a69eb10483c58ef1088a",
+    "url": "/memorygame/static/css/main.001b7b13.chunk.css"
+  },
+  {
+    "revision": "f74ec96f67e6e41a4409615ef0fabadb",
+    "url": "/memorygame/index.html"
+  }
+];
